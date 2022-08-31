1e7f91aed8efb0416922bf5c2d65ddf4c271db5a
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-31 18:41:43 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Its working.');
+})
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
