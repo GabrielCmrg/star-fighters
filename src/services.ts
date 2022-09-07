206a7f05ea88a7a9f2a84edb22b61cf7f809069d
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-07 14:12:39 -0300

    feat: battle now stores the result

--- src/services.ts
+++ src/services.ts
@@ -1,11 +1,48 @@
 import axios from 'axios';
 
+import { createFighter, getFighterByUsername, updateFighterById } from './repositories';
+
 function countStars(data: any[]): number {
   const stars: number = data.reduce(
     (accumulated: number, element: any) => { accumulated + element.stargazers_count },
     0
   );
   return stars;
+}
+
+async function storeResult(result: any, participant1: string, participant2: string) {
+  const participant1Register = {
+    username: participant1,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+  };
+  const participant2Register = {
+    username: participant2,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+  };
+  if (result.winner === participant1) {
+    participant1Register.wins++;
+    participant2Register.losses++;
+  }
+  if (result.draw) {
+    participant1Register.draws++;
+    participant2Register.draws++;
+  }
+  const fighter1 = await getFighterByUsername(participant1);
+  if (!fighter1) {
+    await createFighter(participant1Register);
+  } else {
+    await updateFighterById(participant1Register, fighter1.id);
+  }
+  const fighter2 = await getFighterByUsername(participant2);
+  if (!fighter2) {
+    await createFighter(participant2Register);
+  } else {
+    await updateFighterById(participant2Register, fighter2.id);
+  }
 }
 
 export const battle = async (fighter1: string, fighter2: string) => {
@@ -25,5 +62,6 @@ export const battle = async (fighter1: string, fighter2: string) => {
     loser,
     draw,
   };
+  await storeResult(battleResult, fighter1, fighter2);
   return battleResult;
 }
