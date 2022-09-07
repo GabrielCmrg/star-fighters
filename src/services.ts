27ee35948e74f496c7fb4d9d5f82b85afc19acb2
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-07 10:37:30 -0300

    feat: add function to count total stars from a user

--- src/services.ts
+++ src/services.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
+function countStars(data: any[]): number {
+  const stars: number = data.reduce(
+    (accumulated: number, element: any) => { accumulated + element.stargazers_count },
+    0
+  );
+  return stars;
+}
+
 export const battle = async (fighter1: string, fighter2: string) => {
   try {
     const fighter1Data = await axios.get(`https://api.github.com/users/${fighter1}/repos`);
+    const fighter1StarCount: number = countStars(fighter1Data.data);
     const fighter2Data = await axios.get(`https://api.github.com/users/${fighter2}/repos`);
+    const fighter2StarCount: number = countStars(fighter2Data.data);
   } catch (error: any) {
     console.error(error);
     if (error.response.status === 404) {
